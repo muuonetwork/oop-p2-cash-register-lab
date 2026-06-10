d89dad6eae565731e1b18ce28c5c7d1d21aae553
gideon musee <muuonetwork@gmail.com>
2026-06-10 14:19:10 +0300

    Fix CashRegister: discount, previous_transactions, total setter, print messages

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,42 +1,50 @@
-
 class CashRegister:
     """Simulates a cash register for an e-commerce site."""
 
-    def __init__(self, employee_discount=None):
-        self._employee_discount = employee_discount
+    def __init__(self, discount=0):
+        self._discount = discount
         self._total = 0
         self._items = []
-        self._last_transaction = 0
+        self._previous_transactions = []
 
     @property
-    def employee_discount(self):
-        return self._employee_discount
+    def discount(self):
+        return self._discount
 
     @property
     def total(self):
         return self._total
 
+    @total.setter
+    def total(self, value):
+        self._total = value
+
     @property
     def items(self):
         return self._items
 
+    @property
+    def previous_transactions(self):
+        return self._previous_transactions
+
     def add_item(self, title, price, quantity=1):
         """Add an item to the register with optional quantity."""
         transaction_amount = price * quantity
-        self._last_transaction = transaction_amount
+        self._previous_transactions.append(transaction_amount)
         self._total += transaction_amount
         for _ in range(quantity):
             self._items.append(title)
 
     def apply_discount(self):
-        """Apply the employee discount to the total if one exists."""
-        if self._employee_discount:
-            self._total = int(self._total - (self._total * self._employee_discount / 100))
-            return f"After the discount, the total comes to ${self._total:.2f}."
+        """Apply the discount to the total if one exists."""
+        if self._discount > 0:
+            self._total = self._total - (self._total * self._discount / 100)
+            print(f"After the discount, the total comes to ${self._total:.2f}.")
         else:
-            return "There is no discount to apply."
+            print("There is no discount to apply.")
 
     def void_last_transaction(self):
         """Remove the last transaction amount from the total."""
-        self._total -= self._last_transaction
-        self._last_transaction = 0
+        if self._previous_transactions:
+            last = self._previous_transactions.pop()
+            self._total -= last
